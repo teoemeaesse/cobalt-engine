f140420eafac440bd07fef53fd646c13e4348f56
Tomás da Conceição Marques <tomas.c.marques@tecnico.ulisboa.pt>
2023-12-05 22:28:53 +0000

    Create orthographic and perspective camera.

---
+++ cobalt/core/gfx/camera.cpp
@@ -0,0 +1,83 @@
+//
+// Created by tomas on 05-12-2023.
+//
+
+#include "core/gfx/camera.h"
+#include "camera.h"
+
+
+namespace cobalt {
+    namespace core {
+        Camera::Camera(const glm::vec3 position, const glm::vec2 direction, const float angularSpeed, const float near, const float far)
+            : position(position), direction(direction), angularSpeed(angularSpeed), near(near), far(far)
+        {}
+
+        const glm::mat4x4 Camera::getViewMatrix() const {
+            glm::vec3 center = position + glm::vec3(cos(direction.x) * cos(direction.y), sin(direction.y), sin(direction.x) * cos(direction.y));
+            glm::vec3 z = glm::normalize(position - center);
+            glm::vec3 y = glm::vec3(0.0f, 1.0f, 0.0f);
+            glm::vec3 x = glm::normalize(glm::cross(y, z));
+            y = glm::normalize(glm::cross(z, x));
+
+            return {
+                x.x,                     y.x,                     z.x,                     0.0f,
+                x.y,                     y.y,                     z.y,                     0.0f, 
+                x.z,                     y.z,                     z.z,                     0.0f,
+                -glm::dot(position, x),  -glm::dot(position, y),  -glm::dot(position, z),  1.0f,
+            };
+        }
+
+        void Camera::rotateHorizontal(const float amount)
+        {
+            direction.x += amount;
+        }
+
+        void Camera::rotateVertical(const float amount) {
+            direction.y += amount;
+        }
+
+        void Camera::panDepth(const float amount) {
+            position += glm::vec3(cos(direction.x) * cos(direction.y), sin(direction.y), sin(direction.x) * cos(direction.y)) * amount;
+        }
+
+        void Camera::panHorizontal(const float amount) {
+            position += glm::vec3(cos(direction.x), 0.0f, sin(direction.x)) * amount;
+        }
+
+        void Camera::panVertical(const float amount) {
+            position += glm::vec3(0.0f, 1.0f, 0.0f) * amount;
+        }
+
+        PerspectiveCamera::PerspectiveCamera(glm::vec3 position, glm::vec2 direction, const float fov, const float angularSpeed, const float near, const float far, const float aspectRatio)
+            : Camera(position, direction, angularSpeed, near, far), aspectRatio(aspectRatio)
+        {}
+
+        void PerspectiveCamera::zoom(const float amount) {
+            fov += amount;
+        }
+
+        const glm::mat4x4 PerspectiveCamera::getProjectionMatrix() const {
+            float tan_half_fov = tan(fov / 2.0f);
+
+            return glm::mat4x4 {
+                1.0f / (aspectRatio * tan_half_fov),  0.0f,                0.0f,                               0.0f,
+                0.0f,                                 1.0f / tan_half_fov, 0.0f,                               0.0f,
+                0.0f,                                 0.0f,                (near + far) / (near - far),        -1.0f,
+                0.0f,                                 0.0f,                (2.0f * near * far) / (near - far), 0.0f
+            };
+        }
+
+        OrthographicCamera::OrthographicCamera(const glm::vec3 position, const glm::vec2 direction, const float angularSpeed, const float left, const float right, const float bottom, const float top, const float near, const float far)
+            : Camera(position, direction, angularSpeed, near, far), left(left), right(right), bottom(bottom), top(top)
+        {}
+
+        const glm::mat4x4 OrthographicCamera::getProjectionMatrix() const {
+            return glm::mat4x4 {
+                2.0f / (right - left),           0.0f,                            0.0f,                        0.0f,
+                0.0f,                            2.0f / (top - bottom),           0.0f,                        0.0f,
+                0.0f,                            0.0f,                            2.0f / (near - far),         0.0f,
+                (left + right) / (left - right), (bottom + top) / (bottom - top), (near + far) / (near - far), 1.0f
+            };
+        }
+    }
+}
---
+++ cobalt/core/gfx/camera.h
@@ -0,0 +1,134 @@
+//
+// Created by tomas on 05-12-2023.
+//
+
+#pragma once
+
+#include "core/containers/types.h"
+
+
+namespace cobalt {
+    namespace core {
+        /* A perspective or orthographic camera.
+         */
+        class Camera {
+            friend class PerspectiveCamera;
+            friend class OrthographicCamera;
+            
+            public:
+            /* Destroys the camera.
+             */
+            ~Camera() = default;
+
+            /* Calculates the view matrix of the camera.
+             * @return The view matrix of the camera.
+             */
+            const glm::mat4x4 getViewMatrix() const;
+            /* Calculates the projection matrix of the camera.
+             * @return The projection matrix of the camera.
+             */
+            virtual const glm::mat4x4 getProjectionMatrix() const = 0;
+
+            /* Rotates the camera horizontally by the given amount.
+             * @param amount: The amount to rotate by (radians).
+             */
+            void rotateHorizontal(const float amount);
+            /* Rotates the camera vertically by the given amount.
+             * @param amount: The amount to rotate by (radians).
+             */
+            void rotateVertical(const float amount);
+            /* Pans the camera by the given amount.
+             * @param amount: The amount to pan by.
+             */
+            void panDepth(const float amount);
+            /* Pans the camera horizontally by the given amount.
+             * @param amount: The amount to pan by.
+             */
+            void panHorizontal(const float amount);
+            /* Pans the camera vertically by the given amount.
+             * @param amount: The amount to pan by.
+             */
+            void panVertical(const float amount);
+
+            protected:
+            glm::vec3 position;     // The position of the camera in world space.
+            glm::vec2 direction;    // The direction the camera is facing (radians).
+            float angularSpeed;     // The angular speed of the camera.
+            float near, far;        // The near and far clipping planes.
+            float aspectRatio;      // The aspect ratio of the camera.
+
+            private:
+            /* Creates a new camera.
+             * @param position: The position of the camera in world space.
+             * @param direction: The direction the camera is facing (radians).
+             * @param angularSpeed: The angular speed of the camera.
+             * @param near: The near clipping plane.
+             * @param far: The far clipping plane.
+             */
+            Camera(const glm::vec3 position, const glm::vec2 direction, const float angularSpeed, const float near, const float far);
+        };
+
+        /* A perspective camera.
+         */
+        class PerspectiveCamera : public Camera {
+            public:
+            /* Creates a new perspective camera.
+             * @param position: The position of the camera in world space.
+             * @param direction: The direction the camera is facing (radians).
+             * @param fov: The field of view of the camera.
+             * @param angularSpeed: The angular speed of the camera.
+             * @param near: The near clipping plane.
+             * @param far: The far clipping plane.
+             * @param aspectRatio: The aspect ratio of the camera.
+             */
+            PerspectiveCamera(const glm::vec3 position, const glm::vec2 direction, const float fov, const float angularSpeed, const float near, const float far, const float aspectRatio);
+            /* Destroys the camera.
+             */
+            ~PerspectiveCamera() = default;
+
+            /* Zooms the camera by the given amount.
+             * @param amount: The amount to zoom by (fov radians).
+             */
+            void zoom(const float amount);
+
+            /* Calculates the projection matrix of the camera.
+             * @return The projection matrix of the camera.
+             */
+            const glm::mat4x4 getProjectionMatrix() const override;
+
+            private:
+            float fov;              // The field of view of the camera.
+            float aspectRatio;      // The aspect ratio of the camera.
+        };
+
+        /* An orthographic camera.
+         */
+        class OrthographicCamera : public Camera {
+            public:
+            /* Creates a new orthographic camera.
+             * @param position: The position of the camera in world space.
+             * @param direction: The direction the camera is facing (radians).
+             * @param angularSpeed: The angular speed of the camera.
+             * @param left: The left clipping plane.
+             * @param right: The right clipping plane.
+             * @param bottom: The bottom clipping plane.
+             * @param top: The top clipping plane.
+             * @param near: The near clipping plane.
+             * @param far: The far clipping plane.
+             */
+            OrthographicCamera(const glm::vec3 position, const glm::vec2 direction, const float angularSpeed, const float left, const float right, const float bottom, const float top, const float near, const float far);
+            /* Destroys the camera.
+             */
+            ~OrthographicCamera() = default;
+
+            /* Calculates the projection matrix of the camera.
+             * @return The projection matrix of the camera.
+             */
+            const glm::mat4x4 getProjectionMatrix() const override;
+
+            private:
+            float left, right;      // The left and right clipping planes.
+            float bottom, top;      // The bottom and top clipping planes.
+        };
+    }
+}
