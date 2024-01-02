0900c7353649a16ec63bc609fec3c530e0fcfcff
Tomás da Conceição Marques <tomas.c.marques@tecnico.ulisboa.pt>
2024-01-02 15:46:57 +0000

    Fix setup script.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd lib/glew/auto && sudo make && cd .. && sudo make && sudo make install && sudo make clean
-cd ../glfw && cmake -S . -B build && cd build && sudo make
+cd ../../glfw && cmake -S . -B build && cd build && sudo make
