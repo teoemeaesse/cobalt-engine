77a53f3e40b17923654d9c7638d214e5831ced07
Tomás da Conceição Marques <tomas.c.marques@tecnico.ulisboa.pt>
2024-01-02 16:35:57 +0000

    Update build process for GLFW library

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd lib/glew/auto && sudo make && cd .. && sudo make && sudo make install
-cd ../glfw && cmake -S . -B build && cd build && sudo make
+cd ../glfw && cmake -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF -S . -B build && cd build && sudo make
