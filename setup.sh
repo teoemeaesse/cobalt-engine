af54dca1dd66f24d1189390a84b4ecbe998aeb24
Tomás da Conceição Marques <tomas.c.marques@tecnico.ulisboa.pt>
2024-01-02 15:50:21 +0000

    Switch python command in Makefile for GLEW.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd lib/glew/auto && sudo make && cd .. && sudo make && sudo make install && sudo make clean
+cd lib/glew/auto && sudo make && cd .. && sudo make && sudo make install
 cd ../../glfw && cmake -S . -B build && cd build && sudo make
