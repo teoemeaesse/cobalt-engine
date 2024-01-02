a1ba0ab158e553770b875448eae4b924f749a9ae
Tomás da Conceição Marques <tomas.c.marques@tecnico.ulisboa.pt>
2024-01-02 16:22:31 +0000

    Fix build command in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd lib/glew/auto && sudo make && cd .. && sudo make && sudo make install
-cd ../../glfw && cmake -S . -B build && cd build && sudo make
+cd ../glfw && cmake -S . -B build && cd build && sudo make
