436e5fc6229109f661c5c706850df4ef49fe22db
teomemeaesse <tomasmarques2@gmail.com>
2024-05-03 00:32:24 +0100

    Fix setup script to work with submodules.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-cd lib/glew/auto && sudo make && cd .. && sudo make && sudo make install
-cd ../glfw && cmake -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF -S . -B build && cd build && sudo make
+cd lib
+
+ln -s $(which python3) $(dirname $(which python3))/python
+cd glew/auto && sudo make && cd .. && sudo make && sudo make install
+
+cd ..
+
+cd glfw && cmake -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF -S . -B build && cd build && sudo make
